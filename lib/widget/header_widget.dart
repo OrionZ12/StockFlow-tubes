e6701d6b5a27aea40b84279f7949fde4d8df7e59
karinaqilanainggolan <karinaqila15062005@gmail.com>
2025-12-12 17:33:01 +0700

    tombol notif

--- lib/widget/header_widget.dart
+++ lib/widget/header_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../config/routes.dart';
 import '../theme/app_colors.dart';
 
 class HeaderWidget extends StatelessWidget {
@@ -6,9 +8,9 @@ class HeaderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       children: [
-        Text(
+        const Text(
           "Stock",
           style: TextStyle(
             color: AppColors.textPrimary,
@@ -16,12 +18,40 @@ class HeaderWidget extends StatelessWidget {
             fontSize: 18,
           ),
         ),
-        Text(
+        const Text(
           "Flow",
           style: TextStyle(
             color: AppColors.blueMain,
             fontWeight: FontWeight.bold,
             fontSize: 18,
+          ),
+        ),
+
+        const Spacer(),
+
+        // 🔔 Tombol notifikasi (kanan atas)
+        GestureDetector(
+          onTap: () => context.go(AppRoutes.notification),
+          child: Container(
+            width: 38,
+            height: 38,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
+              border: Border.all(color: AppColors.softBorder),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 3,
+                  offset: Offset(0, 1),
+                ),
+              ],
+            ),
+            child: const Icon(
+              Icons.notifications_none,
+              color: AppColors.blueMain,
+              size: 22,
+            ),
           ),
         ),
       ],
